7556323b5e6650c13bb91b0283a8613154b8557b
KonstantinR0 <kostik-rovin@mail.ru>
2024-10-07 09:43:12 +0500

    Новое задание

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 try:
     number1 = int(input("Введите число: "))
     number2 = int(input("Введите число: "))
-    result = number1/number2
+    result = number1 / number2
 except ValueError:
     print("Введено не число!")
 except ZeroDivisionError:
-    print("Число не равно нулю")
+    print("Нельзя делить на 0!")
 else:
     print(f"Результат: {result}")
 finally:
